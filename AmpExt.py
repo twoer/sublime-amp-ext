09b5d37d4c7f9798e01fac364142134f8b0c36f4
twoer <zhangkun_net@hotmail.com>
2012-07-05 14:45:40 +0800

    更新版本至 0.03

--- AmpExt.py
+++ AmpExt.py
@@ -1,6 +1,6 @@
 import sublime, sublime_plugin
 
-setting = sublime.load_settings("amp_data.sublime-settings")
+setting = sublime.load_settings("amp-data.sublime-settings")
 data = setting.get('data')
 
 class AmpExtCommand(sublime_plugin.TextCommand):
@@ -8,6 +8,13 @@ class AmpExtCommand(sublime_plugin.TextCommand):
 		self.view.window().show_quick_panel(data, self.panel_done)
 
 	def panel_done(self, picked):
+		if(picked <0):
+			return
 		edit = self.view.begin_edit()
-		self.view.insert(edit, self.view.sel()[0].begin(),data[picked][0])
+		begin  = self.view.sel()[0].begin()
+		selLen = len(self.view.substr(self.view.sel()[0]))
+		if(selLen <= 0):
+			self.view.insert(edit, self.view.sel()[0].begin(),data[picked][0])
+		else:
+			self.view.replace(edit, sublime.Region(begin,begin + selLen),data[picked][0])
 	
